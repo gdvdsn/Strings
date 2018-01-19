49bcbf8c5a795df5d8c6973a1f8bc906201f2d00
CS-MA <sdecou@ma.org>
2018-01-19 07:38:32 -0800

    text and template program for string project

--- import_file.py
+++ import_file.py
@@ -1,10 +1,5 @@
 file = open("guido_vonRossum_speech.txt", "r")
 
-def remove_letter(theLetter, theString):
-    new = theString.replace(theLetter, "")
-    print(new)
-    return new
-
 data = file.read()
+print(data)
 
-remove_letter("a", data)
